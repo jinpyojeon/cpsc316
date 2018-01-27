06fc3f881c67c3bf70b2342170c2f6b14202334c
jinpyojeon <jinpyo.jeon@trincoll.edu>
2018-01-27 01:10:35 -0500

    got more fully featured analyzer

--- assign01/scanit.c
+++ assign01/scanit.c
@@ -2,9 +2,18 @@
 #include <ctype.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef enum parseMode { P_NONE, P_FLOAT, P_INT, 
-						 P_IDENTIFIER, P_ESCAPED, P_STRING } ParseMode;
+						 P_IDENTIFIER, P_ESCAPED, P_STRING,
+						 P_OPERATOR } ParseMode;
+
+const char * parseModeStr(ParseMode p) {
+	static char *parseModes[] = {"NONE", "Float", "Int", "Identifier", 
+								 "_", "String", "Operator"};
+
+	return parseModes[p];
+}
 
 bool isOperator(char c) {
 	
@@ -12,8 +21,9 @@ bool isOperator(char c) {
 						'.', '>', '<', '[', ']', 
 						'(', ')', '+', '_', '/', 
 						'*'	};
-
-	for (int i = 0; i < 16; i++) {
+	
+	int i = 0;
+	for (i = 0; i < 16; i++) {
 		if (operators[i] == c) return true;
 	}
 	return false;
@@ -23,12 +33,16 @@ bool isIdentifier(char c) {
 	return isalpha(c) || isdigit(c) || (c == '_');
 }
 
+void resetToken(char * tokenArr, size_t arrSize, int* tokenIndex){
+	memset(tokenArr, 0, sizeof(arrSize));
+	*tokenIndex = 0;
+}
 
-const char * format = "%d: %s \'%c\' found";
-const char * errorFormat = "%d: unknown character %x";
 
+const char * format = "%d: %s \'%s\' found\n";
+const char * errorFormat = "%d: unknown character %c\n";
 
-int main(){
+int main(void){
 	char c;
 	char tokenTypeArr[20];
 	char tokenArr[100];
@@ -38,25 +52,56 @@ int main(){
 	int tokenIndex = 0;
 	ParseMode currMode = P_NONE;
 
-	while (getline(&lineBuf, &lineSize, stdin) > 0){
+	memset(tokenArr, 0, sizeof(tokenArr));
+
+	while (getline(&lineBuf, &lineSize, stdin) > 0) {
 		
 		int i = 0;
-		while (i = 0; i < lineSize; i++) {
+		for (i = 0; i < lineSize - 1; i++) {
 		
 			c = lineBuf[i];
 			
+			// fprintf(stderr, "%c", c);
+			if (!isOperator(c) && !isIdentifier(c) && 
+			    c != '"' && c != ' ' && c != '\0' && 
+				currMode != P_STRING) {
+				fprintf(stderr, errorFormat, lineIndex, c);
+			}
+			
 			if (currMode == P_NONE) {
 				if (isIdentifier(c)) {
-					memset(tokenArr, 0, sizeof(tokenArr));
-					tokenIndex = 0;
+					tokenArr[tokenIndex++] = c;
+					currMode = P_IDENTIFIER;
+				} else if (isOperator(c)) {
+					tokenArr[tokenIndex++] = c;
+					currMode = P_OPERATOR;
+				} else if (c == "\"") {
+					currMode = P_STRING;
+				}
+			}
+			else if (currMode == P_IDENTIFIER){
+				if (isIdentifier(c)) {
 					tokenArr[tokenIndex++] = c;
 				} else {
-					tokenArr[tokenIndex] = '\n';
-					fprintf(stderr, &format, lineIndex, "yo",tokenArr);
+					fprintf(stderr, format, lineIndex, 
+							parseModeStr(currMode),tokenArr);
+					currMode = P_NONE;
+					memset(tokenArr, 0, sizeof(tokenArr));
+					tokenIndex = 0;
 				}
 			}
 			else {
-				tokenArr[tokenIndex++] = c;
+				if (c == ' ' || c == '\n') {
+					tokenArr[tokenIndex] = '\0';
+					fprintf(stderr, format, lineIndex, 
+							parseModeStr(currMode),tokenArr);
+					currMode = P_NONE;
+					memset(tokenArr, 0, sizeof(tokenArr));
+					tokenIndex = 0;
+				}
+				else {
+					tokenArr[tokenIndex++] = c;
+				}
 			}
 			// else if (currMode == ) {
 			//	if (c == ' ') {}
@@ -70,7 +115,7 @@ int main(){
 
 		}
 		
-		free(lineBuf);
+		// free(lineBuf);
 		lineIndex++;		
 	}
 	
